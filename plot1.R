7c864a3c476f6cf72fdadfaf6da46117d6b49840
Jeffrey Kittiyachavalit <jeffreyk@gmail.com>
2014-12-07 13:08:51 +0000

    added comments and made plot background transparent

--- plot1.R
+++ plot1.R
@@ -3,16 +3,22 @@ CreatePlot<-function() {
     dataDownloadLink = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     dataZipFile = "data.zip"
     
+    ## download the zip file if the dataFile doesn't exist
     if(!file.exists(dataFile)) {
         download.file(dataDownloadLink, destfile = dataZipFile, method = "curl")
         unzip(dataZipFile)
     }
+    
+    ## read in data from Feb 1 and 2 2007
     data <- fread(paste("grep '^[12]/2/2007'", dataFile), sep = ";")
     columnNames <- names(fread(dataFile, header = TRUE, sep = ";", nrows = 0))
     setnames(data, columnNames)
     
-    png(file="plot1.png", width=480, height=480)
+    ## save plot as png with specified width and height and make the
+    ## background transparent like the example file in the repository
+    png(file="plot1.png", width=480, height=480, bg="transparent")
     
+    ## plot data
     hist(data$Global_active_power, main="Global Active Power",
          xlab="Global Active Power (kilowatts)", col="red")
     dev.off()
